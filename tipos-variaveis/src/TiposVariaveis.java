b1f9f38272d04e22a7bc7bef715e848788f96673
Christian Souza <christianrsouza@hotmail.com>
2023-01-31 21:30:51 -0300

    Alteração de codigo

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,23 @@
 public class TiposVariaveis {
+    /**
+     * @param args
+     * @throws Exception
+     */
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //TIPOS PRIMITIVOS
+        //estudar a classe String que repesenta o texzto
+        String meuNome = "Christian Souza";
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+        int numero = 5; 
+
+        numero = 21;
+
+        System.out.print(numero);
+
     }
 }
