42fb0e2f3f533046755d7c5ec0b6d5a1a5ef04cf
Christian Souza <christianrsouza@hotmail.com>
2023-01-31 22:16:18 -0300

    Atualizacao de codigo

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -7,16 +7,24 @@ public class Operadores {
         String concatenacao = "?";
         
         concatenacao = 1+1+1+"1";
-        System.out.print(concatenacao);
+        System.out.println(concatenacao);
 
         concatenacao = 1+"1"+1+1;
-        System.out.print(concatenacao);
+        System.out.println(concatenacao);
 
         concatenacao = 1+"1"+1+"1";
-        System.out.print(concatenacao);
+        System.out.println(concatenacao);
 
         concatenacao = "1"+(1+1+1);
-        System.out.print(concatenacao);
+        System.out.println(concatenacao);
+        // aula 3
+
+        int numero = 5;
+        numero = - numero;
+        System.out.println(numero);//deixzando negativo o nemero
+
+        numero = numero *-1;
+        System.out.println(numero);
         
         
     }
