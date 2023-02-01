5d51f1c1361be50fc2504940560f504d0a9b7832
Christian Souza <christianrsouza@hotmail.com>
2023-01-31 22:00:42 -0300

    Atualizacao de codigo

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,24 @@
+public class Operadores {
+    public static void main(String[] args) {
+        String nomeCompleto = "Linguagem " + "Java ";
+        System.out.print(nomeCompleto);
+        
+
+        String concatenacao = "?";
+        
+        concatenacao = 1+1+1+"1";
+        System.out.print(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.print(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+        System.out.print(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+        System.out.print(concatenacao);
+        
+        
+    }
+    
+}
