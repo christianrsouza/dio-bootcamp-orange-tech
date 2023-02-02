a4c633d52826f6ef22b19358f1a37b6469ddac7e
Christian Souza <christianrsouza@hotmail.com>
2023-02-01 22:09:50 -0300

    Atualizacao de codigo

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -25,20 +25,74 @@ public class Operadores {
 
         numero = numero *-1;
         System.out.println(numero);*/
-
-        int a, b;
-        a = 5;
-        b = 6;
+       
+        //a = 5;
+       // b = 6;
         /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
         if(a==b)
         resultado = "verdadeiro";
         else
         resultado = "falso";
         */
-        String resultado = a==b ? "verdadeiro" : "false";
-        System.out.println(resultado);
+        //String resultado = a==b ? "verdadeiro" : "false";
+       // System.out.println(resultado);
+    
+       //aula 5
+        int numero1 = 1;
+        int numero2 = 2;
+
+        if(numero1 > numero2)
+            System.out.print("Numero 1 maior que numero 2");
+
+        if(numero1 < numero2)
+            System.out.print("Numero 1 menor que numero 2");
+
+        if(numero1 >= numero2)
+            System.out.print("Numero 1 maior ou igual que numero 2");
+
+        if(numero1 <= numero2)
+            System.out.print("Numero 1 menor ou igual que numero 2");
+
+        if(numero1 != numero2)
+            System.out.print("Numero 1 é diferente de numero 2");
+         
+
+            //comparacao de string
+        String nome1 = "JAVA";
+        String nome2 = "JAVA";
         
+        System.out.println(nome1 == nome2); //true
+
+        String nome3 = new String("JAVA");
         
+        System.out.println(nome1 == nome3); //false
+
+        String nome4 = nome3;
+
+        System.out.println(nome3 == nome4); //true
+        
+        //equals na parada
+        System.out.println(nome1.equals(nome2)); //??
+        System.out.println(nome2.equals(nome3)); //??
+        System.out.println(nome3.equals(nome4)); //??
+
+        //aula 6 
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        /* Aqui estamos utilizando o operador lógico E para fazer a união de duas 
+        expressões. 
+        É como se estivesse escrito:
+        "Se Condicao1 e Condicao2 forem verdadeiras, executar código"
+        */
+
+        if(condicao1 && condicao2)
+            System.out.print("Os dois valores precisam ser verdadeiros");;
+
+        //Se condicao1 OU condicao2 for verdadeira, executar código.
+        if(condicao1 || condicao2)
+            System.out.print("Um dos valores precisa ser verdadeiro");
     }
 
     
