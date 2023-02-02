e0e6cd40358ce137ca131b5ee367e4e18d518548
Christian Souza <christianrsouza@hotmail.com>
2023-02-01 21:51:46 -0300

    Atualizacao de aula

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,9 +1,9 @@
 public class Operadores {
-    public static void main(String[] args) {
-        String nomeCompleto = "Linguagem " + "Java ";
-        System.out.print(nomeCompleto);
-        
+    
 
+    public static void main(String[] args) {
+        /*String nomeCompleto = "Linguagem " + "Java ";
+        System.out.print(nomeCompleto);      
         String concatenacao = "?";
         
         concatenacao = 1+1+1+"1";
@@ -24,9 +24,23 @@ public class Operadores {
         System.out.println(numero);//deixzando negativo o nemero
 
         numero = numero *-1;
-        System.out.println(numero);
+        System.out.println(numero);*/
+
+        int a, b;
+        a = 5;
+        b = 6;
+        /* EXEMPLO DE CONDICIONAL UTILIZANDO UMA ESTRUTURA IF/ELSE
+        if(a==b)
+        resultado = "verdadeiro";
+        else
+        resultado = "falso";
+        */
+        String resultado = a==b ? "verdadeiro" : "false";
+        System.out.println(resultado);
         
         
     }
+
+    
     
 }
