53fc1fcc22df8a620836a677aed147e19e7a222d
Christian Souza <christianrsouza@hotmail.com>
2023-03-16 20:00:16 -0300

    Codigo de Exercicio

---
+++ exercicios/Exercicio1/src/Desafio2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Desafio2 {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int tamanho = leitor.nextInt();
+        
+        
+    }
+    
+}
