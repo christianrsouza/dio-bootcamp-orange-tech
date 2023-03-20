30e7a9e5397c23113e96486dc4f3ec04fa892dea
Christian Souza <christianrsouza@hotmail.com>
2023-03-19 23:14:04 -0300

    Codigo

--- desafio-dio-poo/src/Main.java
+++ desafio-dio-poo/src/Main.java
@@ -46,7 +46,7 @@ public class Main {
 
         System.out.println("-------");
 
-        Dev devJoao = new Dev();
+        Dev devJoao = new Dev(); 
         devJoao.setNome("Joao");
         devJoao.inscreverBootcamp(bootcamp);
         System.out.println("Conteúdos Inscritos João:" + devJoao.getConteudosInscritos());
